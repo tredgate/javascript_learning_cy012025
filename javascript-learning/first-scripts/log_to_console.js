d85df065325d2edbec9a81f579f41e7981b45952
Petr Fifka <petr.fifka@tredgate.cz>
2025-02-11 23:43:12 +0100

    javascript learning first lecture

---
+++ javascript-learning/first-scripts/log_to_console.js
@@ -0,0 +1,6 @@
+console.log("Programuju v JS!");
+console.log(23 + 23);
+console.log("23" + "23");
+console.log(true);
+console.log(false);
+console.log("petr" + 23);
---
+++ javascript-learning/first-scripts/variables.js
@@ -0,0 +1,19 @@
+//variables.js
+let firstName = "Petr";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+firstName = "Alžběta";
+console.log(firstName);
+firstName = "Denis";
+console.log(firstName);
+
+let secondName;
+secondName = "Novák";
+
+const city = "Praha";
+console.log(city);
+console.log(10 * 5);
